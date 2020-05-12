bc518438c620dbd0db881884813c66d0eecd6c94
nicknguyen <huu@treatmentgps.com>
2020-05-12 22:34:43 +0700

    Output Cart Items

---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,31 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import Colors from '../constants/Colors';
+import { Platform } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import CartScreen from '../screens/shop/CartScreen';
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen,
+    Cart: CartScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+      },
+      headerTitleStyle: {
+        fontFamily: 'open-sans-bold',
+      },
+      headerBackTitleStyle: {
+        fontFamily: 'open-sans',
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+    },
+  }
+);
+
+export default createAppContainer(ProductsNavigator);
